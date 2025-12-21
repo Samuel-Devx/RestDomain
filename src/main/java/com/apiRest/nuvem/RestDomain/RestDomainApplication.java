91855f9e515d74a8557cd92ed51540d2cde60db8
Samuel Devx <samuelldeev@gmail.com>
2025-12-21 10:48:19 -0300

    Initial commit

---
+++ src/main/java/com/apiRest/nuvem/RestDomain/RestDomainApplication.java
@@ -0,0 +1,13 @@
+package com.apiRest.nuvem.RestDomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiRest/nuvem/RestDomain/RestDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiRest.nuvem.RestDomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
